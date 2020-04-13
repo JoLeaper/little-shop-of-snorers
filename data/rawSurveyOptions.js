79648c5ae6af976f4ffe711265207833d2750499
JoLeaper <joleap123@gmail.com>
2020-04-13 15:51:50 -0700

    added in surveyOptions array of objects

---
+++ data/rawSurveyOptions.js
@@ -0,0 +1,162 @@
+const rawSurveyOptions = [
+    {
+        id: 1,
+        name: 'bag',
+        image: '../assets/bag.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 2,
+        name: 'banana',
+        image: '../assets/banana.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 3,
+        name: 'bathroom',
+        image: '../assets/bathroom.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 4,
+        name: 'boots',
+        image: '../assets/boots.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 5,
+        name: 'breakfast',
+        image: '../assets/breaksfast.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 6,
+        name: 'bubblegum',
+        image: '../assets/bubblegum.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 7,
+        name: 'chair',
+        image: '../assets/chair.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 8,
+        name: 'cthulu',
+        image: '../assets/cthulu.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 9,
+        name: 'dog-duck',
+        image: '../assets/dog-duck.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 10,
+        name: 'dragon',
+        image: '../assets/dragon.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 11,
+        name: 'pen',
+        image: '../assets/pen.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 12,
+        name: 'pet-sweep',
+        image: '../assets/scissors.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 13,
+        name: 'scissors',
+        image: '../assets/scissors.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 14,
+        name: 'shark',
+        image: '../assets/bag.sweep.png',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 15,
+        name: 'sweep',
+        image: '../assets/sweep.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 16,
+        name: 'tauntaun',
+        image: '../assets/tauntaun.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 17,
+        name: 'unicorn',
+        image: '../assets/unicorn.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 18,
+        name: 'usb',
+        image: '../assets/usb.gif',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 19,
+        name: 'water can',
+        image: '../assets/water-can.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    },
+    {
+        id: 20,
+        name: 'wine glass',
+        image: '../assets/wine-glass.jpg',
+        timesChosen: 0,
+        timesSeen: 0
+
+    }
+];
